0bedd6a094e3e4d4338ae618eaea7f532b590653
Lucas Metrebian <lucas.metrebian@gmail.com>
2022-09-19 12:12:48 -0300

    se termina ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,56 +1,85 @@
 from tkinter import *
+from tkinter import messagebox
 
 main=Tk()
 main.title("Calculadora")#nombre del titulo
 main.geometry("300x200")#"primero lo ancho y dsp lo alto"
-main.config(bg="dark orange")#color del fondo
+main.config(bg="DarkGray")#color del fondo
 
-def contadorCrecre():
-    con=int(varEntry.get())
-    con+= 1
-    numeroEntry.set(con)
+def Suma():
+    num1=int(numeroEntry.get())
+    num2=int(numeroEntry2.get())
+    numeroEntry3.set(num1+num2)
 
+def Resta():
+    num1=int(numeroEntry.get())
+    num2=int(numeroEntry2.get())
+    numeroEntry3.set(num1-num2)
+
+def Multiplicacion():
+    num1=int(numeroEntry.get())
+    num2=int(numeroEntry2.get())
+    numeroEntry3.set(num1*num2)
+def Division():
+    num1=int(numeroEntry.get())
+    num2=int(numeroEntry2.get())
+    if num2==0:
+        messagebox.showinfo(message="Error - No se puede dividir por 0")
+    else:
+        numeroEntry3.set(num1/num2)
+def Modulo():
+    num1=int(numeroEntry.get())
+    num2=int(numeroEntry2.get())
+    numeroEntry3.set(num1%num2)
+def Reset():
+    numeroEntry.set(0)
+    numeroEntry2.set(0)
+    numeroEntry3.set(0)
 
 numeroEntry=IntVar()
 numeroEntry.set(0)
+numeroEntry2=IntVar()
+numeroEntry2.set(0)
+numeroEntry3=IntVar()
+numeroEntry3.set(0)
 
 varFrame=Frame(main)
 varFrame.pack()
 
 varLabel=Label(varFrame,text="Primer Numero")
-varLabel.grid(row=0,column=0)
+varLabel.grid(row=0,column=0, padx=5, pady=5)
 
 varLabel2=Label(varFrame,text="Segundo Numero")
-varLabel2.grid(row=1,column=0)
+varLabel2.grid(row=1,column=0, padx=5, pady=5)
 
 varLabel3=Label(varFrame,text="Resultado")
-varLabel3.grid(row=2,column=0)
+varLabel3.grid(row=2,column=0, padx=5, pady=5)
 
-varEntry=Entry(varFrame,state="readonly", textvariable= numeroEntry)
-varEntry.grid(row=0,column=1)
+varEntry=Entry(varFrame,textvariable= numeroEntry)
+varEntry.grid(row=0,column=1, padx=5, pady=5)
 
-varEntry2=Entry(varFrame,state="readonly", textvariable= numeroEntry)
-varEntry2.grid(row=1,column=1)
+varEntry2=Entry(varFrame,textvariable= numeroEntry2)
+varEntry2.grid(row=1,column=1, padx=5, pady=5)
 
-varEntry3=Entry(varFrame,state="readonly", textvariable= numeroEntry)
-varEntry3.grid(row=2,column=1)
+varEntry3=Entry(varFrame,state="readonly", textvariable= numeroEntry3)
+varEntry3.grid(row=2,column=1, padx=5, pady=5)
 
-varButton=Button(varFrame,text="+", command=contadorCrecre)
-varButton.grid(row=3,column=0, sticky=EW)
+varButton=Button(varFrame,text="+", command=Suma)
+varButton.grid(row=3,column=0, sticky=EW, padx=5, pady=5)
 
-varButton2=Button(varFrame,text="-", command=contadorCrecre)
-varButton2.grid(row=3,column=1, sticky=EW)
+varButton2=Button(varFrame,text="-", command=Resta)
+varButton2.grid(row=3,column=1, sticky=EW, padx=5, pady=5)
 
-varButton3=Button(varFrame,text="*", command=contadorCrecre)
-varButton3.grid(row=4,column=0, sticky=EW)
+varButton3=Button(varFrame,text="*", command=Multiplicacion)
+varButton3.grid(row=4,column=0, sticky=EW, padx=5, pady=5)
 
-varButton4=Button(varFrame,text="/", command=contadorCrecre)
-varButton4.grid(row=4,column=1, sticky=EW)
+varButton4=Button(varFrame,text="/", command=Division)
+varButton4.grid(row=4,column=1, sticky=EW, padx=5, pady=5)
 
-varButton5=Button(varFrame,text="%", command=contadorCrecre)
-varButton5.grid(row=5,column=0, sticky=EW)
+varButton5=Button(varFrame,text="%", command=Modulo)
+varButton5.grid(row=5,column=0, sticky=EW, padx=5, pady=5)
 
-varButton6=Button(varFrame,text="CLEAR", command=contadorCrecre)
-varButton6.grid(row=5,column=1, sticky=EW)
+varButton6=Button(varFrame,text="CLEAR", command=Reset)
+varButton6.grid(row=5,column=1, sticky=EW, padx=5, pady=5)
 
 main.mainloop()
